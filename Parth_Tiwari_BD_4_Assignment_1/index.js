af9b0320a46f9cf23923068d5746c83ba8fad707
parthR1 <22f3002726@ds.study.iitm.ac.in>
2025-01-06 15:34:38 +0530

    Remove environment variable from port definition

--- Parth_Tiwari_BD_4_Assignment_1/index.js
+++ Parth_Tiwari_BD_4_Assignment_1/index.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 
